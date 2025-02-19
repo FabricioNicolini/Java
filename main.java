eb15dec0adbd688f14a86b6e5c87eb2596efcc32
FabricioNicolini <fabinibarros@gmail.com>
2025-02-19 23:25:12 +0000

    att

--- main.java
+++ main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class main{
     public static void Main ( String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Olá mundo!");
+        System.out.print("");
+        float n= sc.nextInt();
     }
 }   
