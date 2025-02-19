e624b31c814cfd06ff06032b078ccd4a09091528
FabricioNicolini <fabinibarros@gmail.com>
2025-02-19 23:36:56 +0000

    att

--- main.java
+++ main.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 public class main{
     public static void Main ( String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Valor dos custos do espetáculo");
+        System.out.print("Valor dos custos do espetáculo: ");
         float VE=sc.nextFloat();
-        System.out.print("Valor do ingresso");
+        System.out.print("Valor do ingresso: ");
         float VI=sc.nextFloat();
 
         int qI=(int) Math.ceil(VE/VI);
@@ -13,5 +13,4 @@ public class main{
         System.out.println("A quantidade de convites a serem vendidos para que o custo do espetáculo seja alcançado é: "+ qI);
         System.out.println("A quantidade de convites vendidos para obter um lucro de 23% é: "+ q23);
     }
-    
 }   
