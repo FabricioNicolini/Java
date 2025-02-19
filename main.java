f912fe731478ddbc63897a831df149ac8dd394ad
FabricioNicolini <fabinibarros@gmail.com>
2025-02-19 23:29:38 +0000

    att

--- main.java
+++ main.java
@@ -2,7 +2,12 @@ import java.util.Scanner;
 public class main{
     public static void Main ( String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.print("");
-        float n= sc.nextInt();
+        System.out.print("Valor dos custos do espetáculo");
+        float VE=sc.nextFloat();
+        System.out.print("Valor do ingresso")
+        float VI=sc.nextFloat();
+
+        System.out.println("A quantidade de convites a serem vendidos para que o custo do espetáculo seja alcançado é: ")
+        System.out.println("A quantidade de convites vendidos para obter um lucro de 23% é: ");
     }
 }   
