755a6e702621afe9a9f7dbd4c1e3e7f94ff0c691
FabricioNicolini <fabinibarros@gmail.com>
2025-03-26 22:46:38 +0000

    a

---
+++ correcaoSala.java
@@ -0,0 +1,14 @@
+public class correcaoSala{
+
+    public static void main(String[]args){
+
+    }
+
+    public static double[] populaVetor(){
+        double[] precos = new double[10];
+        Scanner sc = new Scanner(System.in);
+        for(int i = 0; i < precos.lenght; i++){
+            
+        }
+    }
+}
