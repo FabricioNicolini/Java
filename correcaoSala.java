ee6401558f43027999d1eec1c86d776c9f263161
FabricioNicolini <fabinibarros@gmail.com>
2025-03-26 23:17:49 +0000

    .

--- correcaoSala.java
+++ correcaoSala.java
@@ -1,33 +1,35 @@
+import java.util.Scanner;
 public class correcaoSala{
 
     public static void main(String[]args){
-        Scanner sc = new Scanner(Sytem.in);
+        Scanner sc = new Scanner(System.in);
         double [] precos = populaVetor();
         System.out.printf("Valor do desconto: ");
         double desconto = sc.nextDouble();
-        aplicacaoDesconto(precos, desconto);
+        
+        aplicaDesconto(precos, desconto);
         imprime(precos);
 
     }
 
     public static void aplicaDesconto(double[] precos, double desconto){
-        for (int i = 0; i < precos.lenght; i++){
+        for (int i = 0; i < precos.length; i++){
             precos[i] = precos[i] - (precos[i] * desconto/100);
         }
     }
 
     public static double[] populaVetor(){
         Scanner sc = new Scanner(System.in);
-        double[] precos = new double[10];
-        for(int i = 0; i < precos.lenght; i++){
+        double[] precos = new double[5];
+        for(int i = 0; i < precos.length; i++){
             System.out.printf("Preco #%d:", (i + 1 ));
-            precos[i] = entrada.nextDouble();
+            precos[i] = sc.nextDouble();
         }
         return precos;
     }
 
     public static void imprime(double[] vetor){
-        for (int i = 0; i < vetor.lenght; i++){
+        for (int i = 0; i < vetor.length; i++){
             System.out.printf("%8.2f", vetor[i]);
         }
         System.out.println();
