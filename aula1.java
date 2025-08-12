c6a1f14e12d692edac013cca9cd9e427841401db
FabricioNicolini <fabinibarros@gmail.com>
2025-08-12 22:59:00 +0000

    .

---
+++ aula1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class aula1{
+        public static void main(String[]args){
+            System.out.println("olá mundo");
+            String nome = "Arthur";
+            System.out.println("olá "+ nome + " ");
+            System.out.println("Bem vindo a seu primeiro programa de java");
+        }
+       
+}
