7b9cfb4caf4fe4539fc0b73941e9d17f58ec73a8
FabricioNicolini <fabinibarros@gmail.com>
2025-08-12 23:47:50 +0000

    .

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[]args){
+        int quantidade= 5;
+        float preco = 10.00f;
+        float compra = (quantidade*preco);
+        System.out.println("qtd: " + quantidade +" ");
+        System.out.println("Valor do produto:  " + preco +" ");
+        System.out.println("O valor da compra é de " + compra +" reais");
+    }
+    public class ex2{
+        double celcius = 50;
+        double fahrenheit = valor;
+    }
+}
